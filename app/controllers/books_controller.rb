173179886a625984e742192766e854f7881d9338
EC2 Default User <ec2-user@ip-172-31-25-119.ap-northeast-1.compute.internal>
2024-06-30 22:31:20 +0000

    [Add]bookのindex

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
-  def new
-    @book = book.new
-  end
+
 
   def index
+    @book = Book.new
+    @book.user_id = current_user.id
   end
 
   def show
@@ -11,5 +11,17 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
+
+  def create
+    book = Book.new(list_params)
+    book.save
+    redirect_to user_path(current_user.id)
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
-  resources :books, only: [:new, :index, :show ,:edit]
-  resources :users, only: [:index, :show, :edit]
-
+  resources :books, only: [:index, :show ,:edit, :create]
+  resources :users, only: [:index, :show, :edit, :new, :create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
